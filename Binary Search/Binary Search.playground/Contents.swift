92fb5dfcb6711efdcdb3942610f53e96d47ca4d3
Duely Yung <dyung2@gmail.com>
2017-10-22 21:45:49 -0500

    Adding binary search algorithm

---
+++ Binary Search/Binary Search.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let numbers = [1,2,4,5,7,8,10]
+
+func binarySearch(array: [Int], numberSearchValue: Int) -> Bool {
+    
+    return false
+}
+
+print(binarySearch(array: numbers, numberSearchValue: 4))
